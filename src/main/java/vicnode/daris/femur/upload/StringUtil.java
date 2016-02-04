9d65eeb14a348bb57d118ec95cc57d7419da86ba
Wilson Liu <wliu5@unimelb.edu.au>
2016-02-04 22:02:07 +1100

    update

---
+++ src/main/java/vicnode/daris/femur/upload/StringUtil.java
@@ -0,0 +1,45 @@
+package vicnode.daris.femur.upload;
+
+public class StringUtil {
+
+    public static String trimNonDigits(String s) {
+        if (s == null) {
+            return null;
+        }
+        s = trimLeadingNonDigits(s);
+        if (s != null) {
+            s = trimTrailingNonDigits(s);
+        }
+        return s;
+    }
+
+    public static String trimLeadingNonDigits(String s) {
+        if (s == null) {
+            return null;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                return s.substring(i);
+            }
+        }
+        return null;
+    }
+
+    public static String trimTrailingNonDigits(String s) {
+        if (s == null) {
+            return null;
+        }
+        for (int i = s.length() - 1; i > 0; i--) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                return s.substring(0, i + 1);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(trimNonDigits("mfc597p"));
+    }
+}
